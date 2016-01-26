8e782ceb9dd46b324b9a80613479a3fd4d19b685
Justas Unity QA <justas@unity3d.com>
2016-01-26 17:13:39 +0200

    Initial commit.

---
+++ Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float amount = 50f;
+
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxis("Horizontal") * amount * Time.deltaTime;
+		float v = Input.GetAxis("Vertical") * amount * Time.deltaTime;
+
+		Debug.Log (h + " " + v);
+
+		rb.AddForce(transform.up * h);
+		rb.AddForce(transform.right * v);
+	}
+}
